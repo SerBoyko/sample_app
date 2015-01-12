336d9df889f34d6d31848564aedf20443084601b
SerBoyko <boykosergey.81@gmail.com>
2015-01-12 14:11:48 +0200

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,14 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
-  end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
+
+end
+
 
 describe "Help page" do
 
@@ -16,6 +23,12 @@ describe "Help page" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
+
   end
 
 describe "About page" do
@@ -23,6 +36,12 @@ describe "About page" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
+
   end
 
 end
